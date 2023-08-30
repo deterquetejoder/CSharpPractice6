6a20d529596b3e6643c5c9161ab6620babb6402f
deterquetejoder <saecularisator@gmail.com>
2023-08-30 22:21:14 +0500

    ts41 complete

--- ts41/Program.cs
+++ ts41/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int Input(string msg)
+{
+    Console.WriteLine(msg);
+ int res = Convert.ToInt32(Console.ReadLine());
+ return res;
+}
+
+int[] InpArray(int size)
+{
+    int[] array = new int[size];
+    for (int i=0 ;i<array.Length;++i )
+    {
+        array [i] = Input($"Enter {1+i} array's element");
+    }
+    return array;
+}
+
+// void PrintArray(int[] array)
+// {
+//     for (int i=0 ;i<array.Length;++i)
+//     {
+//         Console.WriteLine(array[i]);
+//     }
+// }
+
+int PositiveNumbrs(int[] array)
+{
+    int count=0;
+    for(int i=0 ;i<array.Length;++i)
+    {
+        if(array[i]>0) count++;
+        
+    }
+    return count;
+}   
+
+int size = Input("Enter the array's size");
+int[] array = InpArray(size);
+// PrintArray(array);
+Console.WriteLine($"positive numbers amount is {PositiveNumbrs(array)}");
