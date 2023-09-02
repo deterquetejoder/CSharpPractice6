5d10320e5c24e0adf7c41455510afe6fcc1bd571
deterquetejoder <saecularisator@gmail.com>
2023-09-02 20:06:43 +0500

    ts43 done

--- ts43/Program.cs
+++ ts43/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+double Input(string msg)
+{
+    Console.WriteLine(msg);
+ double res = Convert.ToDouble(Console.ReadLine());
+ return res;
+}
+
+double b1=Input("Enter b1");
+double k1=Input("Enter k1");
+double b2=Input("Enter b2");
+double k2=Input("Enter k2");
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.Write($" Cross point: [{x},{y}]");
